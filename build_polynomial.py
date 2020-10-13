fbd02499bb39ac410c66060c7359e9a0d09a219a
dust629 <jss629@hotmail.com>
2020-10-13 15:11:37 +0200

    build_poly

--- build_polynomial.py
+++ build_polynomial.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    poly = np.power(x,0)
+    poly = np.ones(x.shape[0], dtype=int)
     for d in range(1,degree+1):
         poly = np.c_[poly, np.power(x,d)]
     return poly
