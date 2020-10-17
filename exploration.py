b8257bb87151345a177ceb28c64e60b960fc6018
Riccardo Cadei <riccardo.cadei98@gmail.com>
2020-10-17 18:02:21 +0200

    New Poly

--- exploration.py
+++ exploration.py
@@ -3,7 +3,7 @@ import numpy as np
 from helpers import *
 from methods import *
 from process_data import *
-from crossValidation import *
+from crossvalidation import *
 from exploration import *
 from matplotlib import pyplot as plt    
 
